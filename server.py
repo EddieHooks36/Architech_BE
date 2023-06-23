03492195776f9582e78ce81d18c6256c5ea93cfd
RUKtheCROOK <johngarcia053@gmail.com>
2023-06-22 22:40:40 -0700

    Minor change to the bidding

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import config, secrets
 from bson import ObjectId
 import json
 from flask_cors import CORS, cross_origin
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -15,11 +16,6 @@ app.config['SESSION_COOKIE_SECURE'] = True
 # to fix the ObjectId error
 def fix_id(obj):
     obj["_id"] = str(obj["_id"])
-    return obj
-
-# to fix the bidId error
-def fix_bidId(obj):
-    obj["bidId"] = str(obj["_id"])
     return obj
 
 # to get all users
@@ -125,13 +121,18 @@ def bids():
         bid_list.append(fix_id(bid))
     return json.dumps(bid_list)
 
+@app.get("/api/bids/bid/<id>")
+def get_bid_by_id(id):
+    bid = db.Bids.find_one({"_id": ObjectId(id)})
+    return json.dumps(fix_id(bid))
+
 # to get the total number of bids
 @app.get("/api/bids/total")
 def total_bids():
     bids = db.Bids.find()
     bid_list = []
     for bid in bids:
-        bid_list.append(fix_bidId(bid))
+        bid_list.append(fix_id(bid))
     return json.dumps(len(bid_list))
 
 # to add a bid
@@ -143,8 +144,34 @@ def save_bid():
     name = params['name']
     description = params['description']
     bidAmount = params['bidAmount']
-    db.Bids.insert_one({"originalPosterId": originalPosterId, "name": name, "description": description, "bidAmount": bidAmount})
+    image = params['bidImage']
+    db.Bids.insert_one({"originalPosterId": originalPosterId, "name": name, "description": description, "bidAmount": bidAmount, "bidImage": image})
     return jsonify("Bid Added!")
+
+@app.post("/api/bids/bidOnProject")
+def bid_on_project():
+    data = request.json
+    params = data['params']
+    bidId = params['bidId']
+    bidderId = params['bidderId']
+    bidderName = params['bidderName']
+    bidAmount = params['bidAmount']
+    bidDate = datetime.now().strftime("%m/%d/%Y %H:%M")
+    db.Bidders.insert_one({"bidId": bidId, "bidderId": bidderId, "bidderName": bidderName, "bidAmount": bidAmount, "bidDate": bidDate})
+    db.Bids.update_one(
+        {"_id": ObjectId(bidId)},
+          {"$set": {
+                  "bidderId": bidderId,
+                    "bidderName": bidderName,
+                      "bidAmount": bidAmount,
+                        "lastBidDate": bidDate}})
+    return jsonify("Bid Added!")
+
+# returns the current date/time
+@app.get("/api/currentDateTime")
+def current_date_time():
+    currentDateTime = datetime.now().strftime("%m/%d/%Y %H:%M")
+    return jsonify(currentDateTime)
 
         
 
